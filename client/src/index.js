62258eee05321d99a759c5cc6e2fe4ac57335c01
Collin Didier <collin.didier80@gmail.com>
2020-11-12 14:16:40 -0600

    incidental comment

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
-import './styles/globals.css'
+import './styles/App.css'
+// import './styles/globals.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
