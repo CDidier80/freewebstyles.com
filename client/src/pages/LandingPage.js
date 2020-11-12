d8cf535341e0a22635d11b9dcdb0cd0dcdc47d07
Collin Didier <collin.didier80@gmail.com>
2020-11-12 14:18:30 -0600

    add landing page content

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import '../styles/Landing.css'
+import React, { Component } from 'react'
+import '../styles/LandingPage.css'
+import TriangleSvg from "../components/landing-components/TriangleSvg.js"
+import LogInForm from "../components/landing-components/LogInForm.js"
+import GetStartedButton from "../components/landing-components/GetStartedButton"
 
 
-// children is a destructured prop from React
-export default ({ children }) => {
-  console.log(children)
-  return (
-    <div className="landingWrapper">
 
-    </div>
-  )
+class LandingPage extends Component {
+  // console.log(children)
+  constructor(props){
+    super(props)
+    this.state = {
+        key: "value"
+    }
+  }
+
+  render() {
+    return (
+      <div className="landingPage">
+        <TriangleSvg />
+        <LogInForm className="LogInPanel"/>
+        <GetStartedButton />
+      </div>
+    )
+  }
+
 }
+
+export default LandingPage
