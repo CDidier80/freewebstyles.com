a4aada31b01feed2c1b7bd14c9b182ae77afe7de
Collin Didier <collin.didier80@gmail.com>
2020-11-13 18:39:03 -0600

    [UserRouter.js][StyleRouter.js] update routes and model imports

--- routers/sub-routers/StyleRouter.js
+++ routers/sub-routers/StyleRouter.js
@@ -1,9 +1,23 @@
 const Router = require('express').Router()
-const StyleController = require('../Controllers/StyleController')
+const StyleController = require('../../Controllers/StyleController')
 
-Router.post('/styles/:style_id', StyleController.GetStyle)
-Router.get('styles/contribute', StyleController.ContributeStyle)
-Router.delete('styles/deleteone', StyleController.DeleteStyle)
-Router.put('styles/:style_id', StyleController.EditStyle)
+
+
+// Router.get('/test', (req, res) => res.send("StyleRouter.js reached by way of APImeetsServerJs --> AppRouterJs/StyleRouter --> test"))
+
+// Router.get('/StyleControllerJs/editonestyle/test', (req, res) => res.send("StyleRouter.js reached by way of APImeetsServerJs --> AppRouterJs/StyleRouter --> /StyleControllerJs/test"))
+
+Router.post('/StyleControllerJs/poststyle', StyleController.PostStyle)
+
+Router.get('/StyleControllerJs/getonestyle', StyleController.GetOneStyle)
+
+Router.delete('/StyleControllerJs/deleteonestyle', StyleController.DeleteOneStyle)
+
+Router.put('/StyleControllerJs/editonestyle/:style_id', StyleController.EditOneStyle)
+
+
+// Router.get('/styles/:style_id', StyleController.GetStyle)
+// Router.delete('styles/deleteone', StyleController.DeleteStyle)
+// Router.put('styles/:style_id', StyleController.EditStyle)
 
 module.exports = Router
--- routers/sub-routers/UserRouter.js
+++ routers/sub-routers/UserRouter.js
@@ -1,11 +1,8 @@
 const Router = require('express').Router()
-const UserRouter = require('./sub-routers/UserRouter')
-const StyleRouter = require('./sub-routers/StyleRouter')
-const LicenseRouter = require('./sub-routers/LicenseRouter')
 const {getToken, createToken, verifyToken} = require('../../middleware/jwthandler')
 
-Router.get('/:user_id', UserController.GetProfile)
-Router.post('/register', UserController.CreateUser)
-Router.post('/login', UserController.SignInUser, createToken)
-Router.get('/refresh/session/', getToken, verifyToken, UserController.RefreshSession)
+Router.post('/UserControllerJs/register', UserController.CreateUser)
+Router.get('/UserControllerJs/:user_id', UserController.GetProfile)
+Router.post('/UserControllerJs/login', UserController.SignInUser, createToken)
+Router.get('/UserControllerJs/refresh/session/', getToken, verifyToken, UserController.RefreshSession)
 module.exports = Router
