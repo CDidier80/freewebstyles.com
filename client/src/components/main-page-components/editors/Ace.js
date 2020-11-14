dfd5c322cec6a055e84f00301bdcd6964f12fd6c
Collin Didier <collin.didier80@gmail.com>
2020-11-13 18:34:23 -0600

    [Ace.js] remove unneeded comments and function

--- client/src/components/main-page-components/editors/Ace.js
+++ client/src/components/main-page-components/editors/Ace.js
@@ -1,11 +1,15 @@
 import React, {Component} from "react";
-import { render } from "react-dom";
 import AceEditor from "react-ace";
  
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/theme-github";
-
+// import "ace-builds/src-noconflict/theme-terminal";  //dark and red
+// import "ace-builds/src-noconflict/theme-tomorrow_night_blue";  //similar to pycore
+// import "ace-builds/src-noconflict/theme-solarized_dark"; // slightly lighter blues than pycore
+// import "ace-builds/src-noconflict/theme-mono_industrial"; //sleek black/gray/greenish like a dark pycharm
+// import "ace-builds/src-noconflict/theme-cobalt"; // dark blue theme with dark mediterrenean colors
+import "ace-builds/src-noconflict/theme-idle_fingers"; // dark blue theme with dark mediterrenean colors
 
 export default class Ace extends Component {
     // inclusion of props depends on whether index.js passes any
@@ -13,37 +17,25 @@ export default class Ace extends Component {
       super(props)
       this.state = {
           mode: this.props.mode,
-          theme: this.props.theme,
-          name: this.props.name 
+          // theme: this.props.theme,
+          theme: "idle_fingers",
+          name: this.props.name,
+          updateFunction: this.props.updateFunction
+ 
       } 
     }
   
-    // Optional - set the initial state or run any other code needed when the component finishes mounting
-    // componentDidMount (){
-    //   try {
-    //       this.setState((prevState) => ({
-    //          // set initial state here
-    //          ({exampleKey : exampleValue})   
-           
-    //       }))
-    //   } catch (error) {
-    //       console.log(error)
-    //   }
-    // }
-
-    onChange = (newValue) =>  {
-      console.log("change", newValue);
-    }
-  
     render() {
-      const {mode, theme, name} = this.state
+      const {mode, theme, name, updateFunction} = this.state
       return (
         <AceEditor
         mode={mode}
-        theme={theme}
-        onChange={this.onChange}
+        theme={this.state.theme}
+        onChange={updateFunction}
         name={name}
-        editorProps={{ $blockScrolling: true }}
+        editorProps={{ $blockScrolling: true}}
+        // height={this.props.height}
+        // width={this.height.width}
         />
       )}
 }
