d17358eba86891c6ff9465a250a7dbdd263be3cc
Collin Didier <collin.didier80@gmail.com>
2020-11-13 18:35:07 -0600

    [htmlBoilerplate.js] add file with 1 variable string containing string to use as default iframe content

---
+++ client/src/components/main-page-components/editors/htmlBoilerplate.js
@@ -0,0 +1,23 @@
+const htmlBoilerplateStart = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html{
+            background-color: white
+        }
+`
+
+// const htmlBoilerplateStart = 
+// `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <style>
+// `
+module.exports = { htmlBoilerplateStart: htmlBoilerplateStart }
