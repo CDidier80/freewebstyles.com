5951ace32deb7905585d8ca2c962a3d19a014e9c
Collin Didier <collin.didier80@gmail.com>
2020-11-16 13:11:56 -0600

    add crud methods with auth

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -1,8 +1,8 @@
 import ApiClient from './ApiClient'
 
-export const __GetProfile = async (userId) => {
+export const GetProfileService = async (userId) => {
   try {
-    const res = await ApiClient.get(`/users/${userId}`)
+    const res = await ApiClient.get(`/UserRouterJs/UserControllerJs/getprofile/'${userId}`)
     console.log(res.data)
     return res.data
   } catch (error) {
@@ -10,35 +10,39 @@ export const __GetProfile = async (userId) => {
   }
 }
 
-export const __RegisterUser = async (formData) => {
+export const CreateUserService = async (formData) => {
+  console.log('CreateUserService() reached carrying form data:', formData)
   try {
-    const res = await ApiClient.post('/users/register', formData)
+    const res = await ApiClient.post('/UserRouterJs/UserControllerJs/createuser', formData)
     return res.data
   } catch (error) {
     throw error
   }
 }
 
-export const __LoginUser = async (userData) => {
-  console.log('User Data', userData)
+// usercontrollers and middleware will send back a payload and token 
+export const LoginUserService = async (formData) => {
+  console.log("LoginUserService() reached in UserService.js")
+  console.log('Form Data passed into LoginUserService(): ', formData)
   try {
-    const res = await ApiClient.post('/users/login', userData)
-    console.log(res.data)
+    const res = await ApiClient.post('/UserRouterJs/UserControllerJs/login', formData)
+    localStorage.setItem('token', res.data.token)
+    console.log("UserService.js: Response data received from back-end en-route back to client: ", res.data)
     return res.data
   } catch (error) {
     throw error
   }
 }
 
-  // With authentication being integrawted, the client must check that the token created by Axios is valid with each user that is authenticated
+// With authentication being integrated, the client must check that the token created by Axios is valid with each user that is authenticated
 //  we'll need to use a new route 
 //  We need a new function export that we will call __CheckSession
 // This function will connect to our API's (back-end) users/refresh/session endpoint and respond with verified token data if it exists and refresh the current user's session if it does.
 // This service allows a user to remain logged in as they use our application. 
 
-export const __CheckSession = async() => {
+export const CheckSessionService = async() => {
   try{
-    const res = await ApiClient.get('/users/refresh/session')
+    const res = await ApiClient.get('/AppRouterJs/UserRouterJs/UserControllerJs/refresh/session')
     return res.data
   } catch (error) {
     throw error
