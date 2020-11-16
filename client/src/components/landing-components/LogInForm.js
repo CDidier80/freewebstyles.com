66a8ee1dc910572aa33d40349fd2402a191fce74
Collin Didier <collin.didier80@gmail.com>
2020-11-16 13:05:06 -0600

    [LoginForm.js] reformat jss, del unused imports

--- client/src/components/landing-components/LogInForm.js
+++ client/src/components/landing-components/LogInForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -11,8 +10,8 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 // import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles , withStyles} from '@material-ui/core/styles';
-import { positions } from '@material-ui/system'
+import { makeStyles} from '@material-ui/core/styles';
+
 
 function Copyright() {
   return (
@@ -52,76 +51,40 @@ const useStyles = makeStyles((theme) => ({
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
-    "background-color": "rgba(255,255,255,.4)",
+    "background-color": "rgba(255,255,255)",
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+    "background-color": "#2196F3",
+    "font-weight": "bold"
+
   },
 }));
 
-export default function LogInForm() {
-  const classes = useStyles();
-
+export default function LogInForm(props) {
+  const classes = useStyles()
+  const {panelState, updateField, formSubmit} = props
+  let isSigningUp = panelState === "signupLink" ? true : false
   return (
     <Grid container className={classes.root}>
-      {/* <CssBaseline /> */}
       {/* <Grid item xs={false} sm={4} md={7} className={classes.image} /> */}
+
       <Grid component={Paper} square className={classes.paperRoot}>
+
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            {/* <LockOutlinedIcon /> */}
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+          <Avatar className={classes.avatar}>{/* <LockOutlinedIcon /> */}</Avatar>
+          <Typography component="h1" variant="h5">{isSigningUp ? "Sign Up": "Sign in"}</Typography>
+
           <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+            {isSigningUp ? <TextField variant="outlined" margin="normal" required fullWidth id="username" label="Choose Username" name="username" onChange={(e)=>updateField(e, 'username')} autoFocus/> : null}
+            <TextField variant="outlined" margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus onChange={(e)=>updateField(e, 'email')}/>
+            <TextField variant="outlined" margin="normal" required fullWidth id="password" label={isSigningUp ? "Choose Password" : "Password"} name="password" onChange={(e)=>updateField(e, 'password')} autoComplete="current-password" type="password" autoFocus/>
+            <FormControlLabel control={<Checkbox value="remember" color="primary" />}label="Remember me"/>
+            <Button type="button" onClick={(e)=>formSubmit(e)} fullWidth variant="contained" color="primary" className={classes.submit}>{isSigningUp ? "CONFIRM AND SIGN IN" : "SIGN IN"}</Button>
+              {/* <Grid item xs>
+                <Link href="#" variant="body2">Forgot password?</Link>
+              </Grid> */}
+            {!isSigningUp ? <Link href="#" variant="body1">{"Don't have an account? Sign Up"}</Link> : null}
             <Box mt={5}>
               <Copyright />
             </Box>
