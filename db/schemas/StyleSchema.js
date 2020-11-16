fba8e3c15c6691ce4ce9b599bec28935203ef451
Collin Didier <collin.didier80@gmail.com>
2020-11-16 13:13:41 -0600

    add notes

--- db/schemas/StyleSchema.js
+++ db/schemas/StyleSchema.js
@@ -2,9 +2,9 @@ const { Schema } = require('mongoose')
 
 module.exports = new Schema(
   {
-    html: {type: String, required: true, index: true},  
-    css: {type: String, default: 0, required: true},
-    creator: {type: Schema.Types.ObjectId,ref: 'styles', required: true},
+    html: {type: String, required: true, index: true},  // user html
+    css: {type: String, default: 0, required: true},    // user css
+    creator: {type: Schema.Types.ObjectId, ref: 'styles', required: true},
     // creator: {type: String, required: true},   // a string version just for testing the posting of styles without needing a reference
     style_name: {type: String,required: true, index: true},  // style names should not be duplicated
     up_votes: {type: Number,default: 0,},
