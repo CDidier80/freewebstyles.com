ad0b1a618e7da83f0d4fc0175ee59d886b7c1bd6
Collin Didier <collin.didier80@gmail.com>
2020-11-17 12:51:40 -0600

    fixed reference to an incorrect model

--- db/schemas/StyleSchema.js
+++ db/schemas/StyleSchema.js
@@ -4,11 +4,10 @@ module.exports = new Schema(
   {
     html: {type: String, required: true, index: true},  // user html
     css: {type: String, default: 0, required: true},    // user css
-    creator: {type: Schema.Types.ObjectId, ref: 'styles', required: true},
+    creator: {type: Schema.Types.ObjectId, ref: 'users', required: true},
     // creator: {type: String, required: true},   // a string version just for testing the posting of styles without needing a reference
     style_name: {type: String,required: true, index: true},  // style names should not be duplicated
     up_votes: {type: Number,default: 0,},
-    license_name: {type: String, required: true},
     tags: [{type: String, required: true}],
 
     // for seeding content to db from 3rd party sources without accounts
--- routers/sub-routers/LicenseRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express').Router()
-const LicenseController = require('../Controllers/LicenseController')
-
-Router.post('/:license_id', LicenseController.GetLicense)
-Router.get('/addlicense', LicenseController.CreateLicenseOption)
-
-module.exports = Router
