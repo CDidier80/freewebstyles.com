7ee251455908ff19f6b6613b2d68f42431a907e1
Collin Didier <collin.didier80@gmail.com>
2020-11-17 16:23:19 -0600

    reformatted

--- db/schemas/UserSchema.js
+++ db/schemas/UserSchema.js
@@ -2,21 +2,11 @@ const { Schema } = require('mongoose')
 
 module.exports = new Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      index: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      index: true
-    },
-    password_digest: {
-      type: String,
-      required: true
-    }
+    username: { type: String, required: true, index: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password_digest: { type: String, required: true },
+    liked_styles: [{ type: String, index: true }]
+
   },
   { timestamps: true }
 )
