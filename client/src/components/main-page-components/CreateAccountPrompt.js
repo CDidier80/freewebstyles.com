959289ae6eac4d1285cf9a7ec178819a38a6220f
Collin Didier <collin.didier80@gmail.com>
2020-11-17 12:47:43 -0600

    del copy/pasted material

--- client/src/components/main-page-components/CreateAccountPrompt.js
+++ client/src/components/main-page-components/CreateAccountPrompt.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react'
-import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-// import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import tagOptions from '../../data-exports/allTagOptions'
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import '../../styles/SubmitForm.css'
+import '../../styles/CreateAccountPrompt.css'
 
-import IconButton from '@material-ui/core/IconButton';
-// import ListItemText from '@material-ui/core/ListItemText';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ClearIcon from '@material-ui/icons/Clear';
-import { NavLink } from 'react-router-dom';
 
 
 export default class SubmitForm extends Component {
@@ -23,26 +9,10 @@ export default class SubmitForm extends Component {
         // console.log(this.props.userCSS)
         this.state = {
             toggleABoolean: props.toggleABoolean,
-            userCSS: props.userCSS,
-            userHTML: props.userHTML,
-            open: true,
-            styleName: '',
-            license: "",
-            tagOptions: tagOptions.allTagOptions,
-            sourceLinks: [],
-            dense: false,
-            tagOneValue: "",
-            tagTwoValue: "",
-            userID: ""
-
-
+            goToSignupPage: props.goToSignupPage
         }
     }
     
-    goToSignupPage = (e) => {
-        e.stopPropagation()
-        this.props.history.push('/SignUp')
-    }
 
     render() {
         // removed from MenuItems: style={getStyles(name, personName, theme)}
@@ -50,13 +20,17 @@ export default class SubmitForm extends Component {
         // MUST PUT IN A HANDLE CHANGE FOR SELECT
         // select documentation: https://material-ui.com/components/selects/#select
         
-        const {styleName, tags, tagOptions, sourceLinks, toggleABoolean, dense, sourceCount, tagOneValue, tagTwoValue, tagThreeValue} = this.state
+        const {toggleABoolean, goToSignupPage} = this.state
+        console.log("this.state.goToSignupPage logged at CreateAccountPrompt.js render: ", this.state.goToSignupPage)
         return (
             <div className="customBackdrop" id="backdrop" onClick={(e) => toggleABoolean(e, "isCreateAccountPromptVisible")}>
-                    <p className="pTagPrompt">Sign in to share your style!</p>
-
-                    <NavLink className="linkToSignUp" onClick={(e)=>this.goToSignupPage(e)}>Sign In</NavLink>
-                    <NavLink className="linkToSignUp" onClick={(e)=>this.goToSignupPage(e)}>Create Account</NavLink>
+                <div className="prompt">
+                    <p className="pTagPrompt">Sign in to share your style</p>
+                    <div className="linkContainer">
+                        <p className="signInLink" onClick={(e)=>goToSignupPage(e)}>Sign In</p>
+                        <p className="signUpLink" onClick={(e)=>goToSignupPage(e)}>Create Account</p>
+                    </div>
+                </div>
             </div>
 
 
