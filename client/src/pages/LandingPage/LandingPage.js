eab53539274952655cf9d18fcc3a7313cb98fd7c
Collin Didier <collin.didier80@gmail.com>
2021-03-02 19:55:05 -0600

    dependencies added landing page cleared of old, unused code

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -6,111 +6,30 @@ import { CreateUserService, LoginUserService } from '../services/UserService'
 import { LandingPageLogs } from "./logs"
 
 class LandingPage extends Component {
-  // console.log(children)
-  constructor(props){
-    super(props)
-    this.state = {
-        toggleAuthenticated: props.toggleAuthenticated,
-        verifyTokenValid: props.verifyTokenValid,
-        authenticated: props.authenticated,
-        currentUser: props.currentUser,
-        eventTarget: "",
-        username: "",
-        email: "",
-        password: ""
-    }
-  }
 
-  activateForm = (e) => {
-    e.preventDefault() 
-    console.log('clicked')
-      this.setState({eventTarget: e.target.className})
-    }
 
   goToMainPage = () => {
     this.props.history.push('/main')
   }
 
-  submitSignUp = (e) => {
-    // e.preventDefault()
-    const formData = {username: this.state.username, email: this.state.email, password: this.state.password}
-    // console.log(formData)
-    CreateUserService(formData)
-  }
-
-  submitLogIn = async (e) => {
-    console.log('submitLogin() fired')
-    const {toggleAuthenticated, email, password} = this.state
-    e.preventDefault()
-    const formData = {email: email, password: password}
-    console.log("formData sent to back-end: ", formData)
-    const responseData =  await LoginUserService(formData)
-    console.log("Response received: ",responseData)
-    console.log("Username received as part of responseData: ", responseData.user.username)
-    toggleAuthenticated(true, responseData.user.username, ()=>this.props.history.push('/main'))
-  }
-
-  updateField = (event, fieldToUpdate) => {
-    switch (event.target.id) {
-      case "username":
-        this.setState({username: event.target.value})
-        break
-      case "email":
-        this.setState({email: event.target.value})
-        break
-      case "password":
-        this.setState({password: event.target.value})
-        break
-      default: 
-        console.log('updateField() switch statement originating in LandingPage.js had no matching cases.')
-    }
-  }
 
   render() {
 
-    const { eventTarget, currentUser, authenticated } = this.state
-    LandingPageLogs(this.state, this.props, currentUser, authenticated)
 
     return (
       <div className="landingPage">
         <TriangleSvg />
-        {eventTarget === "loginLink" && (
-          <LogInForm 
-            styleChoice={"landingStyles"} 
-            className="LogInPanel" 
-            panelState={eventTarget} 
-            formSubmit={this.submitLogIn} 
-            updateField={this.updateField}
-          /> 
-        )}
-        {eventTarget === "signupLink" && (
-          <LogInForm 
-            styleChoice={"landingStyles"} 
-            className="LogInPanel" 
-            panelState={eventTarget} 
-            formSubmit={this.submitSignUp} 
-            updateField={this.updateField}
-          /> 
-        )}
 
         <div className="landingPageGreeting landingPageLeft">
           <div className="webstylesWrapper">          
             <h1 className="freeWord bothWords ">FREE</h1>
             <h1 className="webstylesWord bothWords ">WEB STYLES</h1>
           </div>
+
+
           <div className='actionContainer'>
-              <p 
-                className='loginLink' 
-                onClick={(e)=>this.activateForm(e)}
-              >
-                Sign In
-              </p>
-              <p 
-                className='signupLink' 
-                onClick={(e)=>this.activateForm(e)}
-              >
-                Sign Up
-              </p>
+              <p className='loginLink' > Sign In </p>
+              <p className='signupLink' > Sign Up </p>
               <button 
                 className="getStartedButton" 
                 onClick={()=>this.goToMainPage()}
