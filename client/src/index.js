870b1d43a8608790e234e564da248a89e766544f
Collin Didier <collin.didier80@gmail.com>
2021-03-02 19:42:25 -0600

    reformat imports and fix code spacing

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
+import React from 'react'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
