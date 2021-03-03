cae4013da83fb483a1dd143ae95786e1ffefbb24
Collin Didier <collin.didier80@gmail.com>
2021-03-02 19:57:54 -0600

    del mongodb imports for conversion to sql

--- server.js
+++ server.js
@@ -4,39 +4,29 @@ const logger = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const connection = require('./db/connection')
 
 const PORT = process.env.PORT || 3003
 const app = express()
 
-// Initialize Middleware
 app.use(logger('dev'))
-app.use(helmet())
-// app.use(helmet({ contentSecurityPolicy: false })); --> deployment only, replaces above
+app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// Initialize Middleware
-
-app.disable('X-Powered-By')    // addresses huge security hole in express, conceals which api you're using. 
-
-
-// app.get('/', (req, res) => res.send({ msg: 'Server Working' }))  --> for dev testing only
+app.disable('X-Powered-By')    
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.use('/APImeetsServerJs', AppRouter)
 
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"))   ----> for deployment only
-// );
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html")) 
+)
 
 
 app.listen(PORT, async () => {
   try {
-    await connection
-    console.log('Database Connected')
     console.log(`App listening on port: ${PORT}`)
   } catch (error) {
-    throw new Error('Connection Error')
+    throw new Error('Server Error')
   }
 })
