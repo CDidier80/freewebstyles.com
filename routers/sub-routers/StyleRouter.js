cb6c43d29cad0b30033e668198b4057c483b10b3
Collin Didier <collin.didier80@gmail.com>
2021-03-02 19:57:38 -0600

    del commented code

--- routers/sub-routers/StyleRouter.js
+++ routers/sub-routers/StyleRouter.js
@@ -2,13 +2,6 @@ const Router = require('express').Router()
 const StyleController = require('../../Controllers/StyleController')
 
 
-
-Router.get('/test', (req, res) => res.send("StyleRouter.js reached by way of APImeetsServerJs --> AppRouterJs/StyleRouter --> test"))
-
-// Router.get('/StyleControllerJs/editonestyle/test', (req, res) => res.send("StyleRouter.js reached by way of APImeetsServerJs --> AppRouterJs/StyleRouter --> /StyleControllerJs/test"))
-
-
-
 Router.post('/StyleControllerJs/poststyle', StyleController.PostStyle)
 
 Router.get('/StyleControllerJs/getonestyle/:style_name', StyleController.GetOneStyle)
@@ -18,8 +11,5 @@ Router.delete('/StyleControllerJs/deleteonestyle', StyleController.DeleteOneStyl
 Router.put('/StyleControllerJs/editonestyle/:style_id', StyleController.EditOneStyle)
 
 
-// Router.get('/styles/:style_id', StyleController.GetStyle)
-// Router.delete('styles/deleteone', StyleController.DeleteStyle)
-// Router.put('styles/:style_id', StyleController.EditStyle)
 
 module.exports = Router
