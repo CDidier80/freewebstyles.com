316b4adb28712b519d6ad7f45f07629d681ac06e
Collin Didier <collin.didier80@gmail.com>
2021-03-03 20:53:52 -0600

    del semicolons

--- client/src/pages/MainPage/components/Ace.js
+++ client/src/pages/MainPage/components/Ace.js
@@ -1,15 +1,15 @@
 import React, {Component} from "react";
 import AceEditor from "react-ace";
  
-import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/mode-css";
-import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/mode-html"
+import "ace-builds/src-noconflict/mode-css"
+import "ace-builds/src-noconflict/theme-github"
 // import "ace-builds/src-noconflict/theme-terminal";  //dark and red
 // import "ace-builds/src-noconflict/theme-tomorrow_night_blue";  //similar to pycore
 // import "ace-builds/src-noconflict/theme-solarized_dark"; // slightly lighter blues than pycore
 // import "ace-builds/src-noconflict/theme-mono_industrial"; //sleek black/gray/greenish like a dark pycharm
 // import "ace-builds/src-noconflict/theme-cobalt"; // dark blue theme with dark mediterrenean colors
-import "ace-builds/src-noconflict/theme-idle_fingers"; // dark blue theme with dark mediterrenean colors
+import "ace-builds/src-noconflict/theme-idle_fingers"// dark blue theme with dark mediterrenean colors
 
 export default class Ace extends Component {
     // inclusion of props depends on whether index.js passes any
