9b74beafe5ab6cc18c76e707f9440c9e37d2723e
Collin Didier <collin.didier80@gmail.com>
2021-03-03 20:54:52 -0600

    component for landing page hero title

---
+++ client/src/pages/LandingPage/components/FreeWebStyles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FreeWebStyles = () => {
+    return (
+        <div className="webstylesWrapper">          
+            <h1 className="freeWord bothWords ">FREE</h1>
+            <h1 className="webstylesWord bothWords ">WEB STYLES</h1>
+        </div>
+    )
+}
+
+export default FreeWebStyles
