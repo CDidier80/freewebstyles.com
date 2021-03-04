5ba2cb3416733b20bf41157e5bbf7e1d91eb8774
Collin Didier <collin.didier80@gmail.com>
2021-03-03 20:56:38 -0600

    convert to es6 import syntax

--- client/src/modules/htmlBoilerplate.js
+++ client/src/modules/htmlBoilerplate.js
@@ -1,4 +1,4 @@
-const htmlBoilerplateStart = 
+export const htmlBoilerplate = 
 `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,14 +10,3 @@ const htmlBoilerplateStart =
             background-color: white
         }
 `
-
-// const htmlBoilerplateStart = 
-// `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-//     <style>
-// `
-module.exports = { htmlBoilerplateStart: htmlBoilerplateStart }
