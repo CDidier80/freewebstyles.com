520d83aa82b5f904cf0e7ad260c893b86b67ae81
Collin Didier <collin.didier80@gmail.com>
2021-03-03 20:56:05 -0600

    convert to functional component spread content into subcomponents del unused imports

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,47 +1,23 @@
-import React, { Component } from 'react'
-import '../styles/LandingPage.css'
-import TriangleSvg from "./landing-components/TriangleSvg.js"
-import LogInForm from "../components/landing-components/LogInForm.js"
-import { CreateUserService, LoginUserService } from '../services/UserService'
-import { LandingPageLogs } from "./logs"
+import GetStylishButton from './components/GetStylishButton'
+import FreeWebStyles from "./components/FreeWebStyles"
+import TriangleSvg from "./components/TriangleSvg.js"
+import React from 'react'
+import './landingPage.css'
 
-class LandingPage extends Component {
-
-
-  goToMainPage = () => {
-    this.props.history.push('/main')
-  }
-
-
-  render() {
-
+const LandingPage = (props) => {
 
     return (
       <div className="landingPage">
         <TriangleSvg />
 
         <div className="landingPageGreeting landingPageLeft">
-          <div className="webstylesWrapper">          
-            <h1 className="freeWord bothWords ">FREE</h1>
-            <h1 className="webstylesWord bothWords ">WEB STYLES</h1>
-          </div>
 
-
-          <div className='actionContainer'>
-              <p className='loginLink' > Sign In </p>
-              <p className='signupLink' > Sign Up </p>
-              <button 
-                className="getStartedButton" 
-                onClick={()=>this.goToMainPage()}
-              >
-                GET STARTED
-              </button>
-          </div>
+          <FreeWebStyles  />
+          <GetStylishButton {...props} />
         </div>
       </div>
     )
   }
 
-}
 
 export default LandingPage
